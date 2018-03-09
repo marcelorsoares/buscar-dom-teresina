5e416e51f5f1910dfcd66eced6c842c42eebd97c
Marcelo Soares <marcelo.rs@gmail.com>
2018-03-09 02:19:35 -0300

    first commit

---
+++ busca_diario_oficial_teresina.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+
+import requests
+import urllib2
+from lxml import html
+import PyPDF2
+import re
+import os
+
+# PALAVRAS A SEREM BUSCADAS
+palavras = ['concurso',] 
+# #########################
+
+# Cria uma pasta pdfs para armazenar os pdfs baixos
+if not os.path.exists('pdfs'):
+	os.makedirs('pdfs')
+
+# Cria uma pasta founds para armazenar as paginas dos pdf que contem as palavras buscadas
+if not os.path.exists('founds'):
+	os.makedirs('founds')
+
+url_address = 'lista_diario.php'
+
+while(url_address != ''):
+	url = requests.get('{}/{}'.format('http://www.dom.teresina.pi.gov.br',url_address))
+	tree = html.fromstring(url.content)
+	pdfs = tree.xpath('//table[@class="table table-bordered table-striped"]/tbody/tr/td/ul/li/a/@href')
+
+	for pdf in pdfs:
+		print(pdf)
+		
+		file_name = pdf.split('/')[-1]
+		date = file_name.split('-')[1]
+
+		year = date[4:8]
+		month = date[2:4]
+		day = date[0:2]
+		dom = file_name.split('-')[0]
+		
+		year_folder = 'pdfs/{}'.format(year)
+		month_folder = '{}/{}'.format(year_folder, month)
+		day_folder = '{}/{}'.format(month_folder, day)
+
+		if not os.path.exists(year_folder):
+			os.makedirs(year_folder)
+
+		if not os.path.exists(month_folder):
+			os.makedirs(month_folder)
+
+		if not os.path.exists(day_folder):
+			os.makedirs(day_folder)
+
+		local_pdf = '{}/{}'.format(day_folder,file_name)
+
+		if not os.path.exists(local_pdf):
+			response = urllib2.urlopen(urllib2.Request(pdf)).read()
+			with open(local_pdf, "wb") as handle:
+				handle.write(response)
+
+		pdf_file = open(local_pdf)
+			
+		file_reader = PyPDF2.PdfFileReader(pdf_file)
+
+		for page_number in xrange(file_reader.numPages):
+
+			page = file_reader.getPage(page_number)
+			text = page.extractText()
+
+			for palavra in palavras:
+
+				if re.search(palavra, text, re.IGNORECASE):
+					writer = PyPDF2.PdfFileWriter()
+					writer.addPage(page)
+					
+					# Cria um arquivo com o padrão: DOM123-01021980-1.pdf dentro da pasta founds
+					outputstream = open("{}/{}-{}-{}-{}.pdf".format('founds',palavra,dom,date,page_number), "wb")
+					writer.write(outputstream)
+					outputstream.close()
+
+	url_address = tree.xpath('//p[@class="box-paginacao"]/span[@class="label"]/a/@href')[0]
