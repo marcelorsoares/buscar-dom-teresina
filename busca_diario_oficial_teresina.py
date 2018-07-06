b48f615e3d5f04c438973453d03f4f9413f7f50a
Marcelo Soares <marcelo.rs@gmail.com>
2018-07-05 22:42:52 -0300

    correcao na paginacao

--- busca_diario_oficial_teresina.py
+++ busca_diario_oficial_teresina.py
@@ -7,9 +7,13 @@ import PyPDF2
 import re
 import os
 
+
 # PALAVRAS A SEREM BUSCADAS
-palavras = ['concurso',] 
+palavras = ['marcelo'] 
 # #########################
+
+# QUANTIDAD DE PÁGINAS A SEREM PECORRIDAS
+paginas = 30
 
 # Cria uma pasta pdfs para armazenar os pdfs baixos
 if not os.path.exists('pdfs'):
@@ -20,9 +24,9 @@ if not os.path.exists('founds'):
 	os.makedirs('founds')
 
 url_address = 'lista_diario.php'
-
-while(url_address != ''):
-	url = requests.get('{}/{}'.format('http://www.dom.teresina.pi.gov.br',url_address))
+for page in range(1,paginas):
+	print(u'Página {}'.format(page))
+	url = requests.get('{}/{}?pagina={}'.format('http://www.dom.teresina.pi.gov.br',url_address, page))
 	tree = html.fromstring(url.content)
 	pdfs = tree.xpath('//table[@class="table table-bordered table-striped"]/tbody/tr/td/ul/li/a/@href')
 
@@ -52,29 +56,32 @@ while(url_address != ''):
 
 		local_pdf = '{}/{}'.format(day_folder,file_name)
 
-		if not os.path.exists(local_pdf):
-			response = urllib2.urlopen(urllib2.Request(pdf)).read()
-			with open(local_pdf, "wb") as handle:
-				handle.write(response)
+		try:
+			if not os.path.exists(local_pdf):
 
-		pdf_file = open(local_pdf)
-			
-		file_reader = PyPDF2.PdfFileReader(pdf_file)
+				response = urllib2.urlopen(urllib2.Request(pdf.replace(' ','%20'))).read()
+				with open(local_pdf, "wb") as handle:
+					handle.write(response)
 
-		for page_number in xrange(file_reader.numPages):
+			pdf_file = open(local_pdf)
+				
+			file_reader = PyPDF2.PdfFileReader(pdf_file)
 
-			page = file_reader.getPage(page_number)
-			text = page.extractText()
+			for page_number in xrange(file_reader.numPages):
 
-			for palavra in palavras:
+				page = file_reader.getPage(page_number)
+				text = page.extractText()
 
-				if re.search(palavra, text, re.IGNORECASE):
-					writer = PyPDF2.PdfFileWriter()
-					writer.addPage(page)
-					
-					# Cria um arquivo com o padrão: DOM123-01021980-1.pdf dentro da pasta founds
-					outputstream = open("{}/{}-{}-{}-{}.pdf".format('founds',palavra,dom,date,page_number), "wb")
-					writer.write(outputstream)
-					outputstream.close()
+				for palavra in palavras:
 
-	url_address = tree.xpath('//p[@class="box-paginacao"]/span[@class="label"]/a/@href')[0]
+					if re.search(palavra, text, re.IGNORECASE):
+						writer = PyPDF2.PdfFileWriter()
+						writer.addPage(page)
+						
+						# Cria um arquivo com o padrão: DOM123-01021980-1.pdf dentro da pasta founds
+						outputstream = open("{}/{}-{}-{}-{}.pdf".format('founds',palavra,dom,date,page_number), "wb")
+						writer.write(outputstream)
+						outputstream.close()
+
+		except urllib2.HTTPError:
+				print('erro ao baixar o arquivo {}'.format(pdf))
